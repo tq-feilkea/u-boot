4665019fb5140d15e2ee8327e7ccc4504962e9c4
Nora Schiffer <nora.schiffer@ew.tq-group.com>
2026-03-25 10:12:20 +0100

    board: tq: common: add sysinfo helpers
    Add a number of helpers based on the tq_eeprom sysinfo driver and
    related hardware detection features. The helpers set up the U-Boot
    environment and fixup the OS Device Tree.
    
    Signed-off-by: Nora Schiffer <nora.schiffer@ew.tq-group.com>
    Signed-off-by: Alexander Feilke <alexander.feilke@ew.tq-group.com>

---
+++ board/tq/common/tq_sysinfo.c
@@ -0,0 +1,32 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
+/*
+ * Common sysinfo helpers for TQ-Systems SOMs
+ *
+ * Copyright (c) 2020-2026 TQ-Systems GmbH <u-boot@ew.tq-group.com>
+ * D-82229 Seefeld, Germany.
+ * Author: Nora Schiffer
+ */
+
+#include <env.h>
+#include <sysinfo/tq_eeprom.h>
+
+#define MAX_NAME_LENGTH	80
+
+void tq_common_sysinfo_setup(void)
+{
+	struct udevice *sysinfo;
+	char buf[MAX_NAME_LENGTH] = "";
+	int ret;
+
+	ret = sysinfo_get_and_detect(&sysinfo);
+	if (ret) {
+		pr_err("Failed to get sysinfo data: %d\n", ret);
+		return;
+	}
+
+	if (!sysinfo_get_str(sysinfo, SYSID_TQ_MODEL, sizeof(buf), buf))
+		env_set_runtime("boardtype", buf);
+
+	if (!sysinfo_get_str(sysinfo, SYSID_TQ_SERIAL, sizeof(buf), buf))
+		env_set_runtime("serial#", buf);
+}
---
+++ board/tq/common/tq_sysinfo.h
@@ -0,0 +1,18 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+/*
+ * Common sysinfo helpers for TQ-Systems SOMs
+ *
+ * Copyright (c) 2025-2026 TQ-Systems GmbH <u-boot@ew.tq-group.com>
+ * D-82229 Seefeld, Germany.
+ * Author: Nora Schiffer
+ */
+
+#ifndef __TQ_SYSINFO_H
+
+#ifndef CONFIG_XPL_BUILD
+
+void tq_common_sysinfo_setup(void);
+
+#endif
+
+#endif /* __TQ_SYSINFO_H */
